b79a73da5eeb362a75c6c42e41e50be292da9b39
Christian Lawson-Perfect <christianperfect@gmail.com>
2024-01-30 15:48:00 +0000

    what was actually on the server in December 2022
    It looks like I had a half-finished change on my desktop, using the
    latest version of the elm graph-app which I was updating for use with
    Numbas, and a slightly older version on the web, using the old version
    of the component.
    
    I don't want to spend time today making it work, so I've fetched the
    files that were on the web.

--- graph_app/embed-graph-app.js
+++ graph_app/embed-graph-app.js
@@ -21,9 +21,6 @@ var observer = new MutationObserver(function (mutations) {
             Array
                 .from(mutation.addedNodes)
                 .forEach(function (node) {
-                    if(node.nodeType != node.ELEMENT_NODE || node.tagName.toLowerCase()!='svg') {
-                        return;
-                    }
                     node.addEventListener('mousemove', function (event) {
                         const t = getsvg(event);
                         if(!t) {
@@ -66,80 +63,17 @@ document.addEventListener('mousemove',e => {
     }
 })
 
-function truthy_attribute(value) {
-    return value !== null && (value.toLowerCase()=='true' || value=='');
-}
-
 class GraphAppElement extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({mode: 'open'});
-        this.app_created = new Promise((resolve,reject) => {
-            this.resolve_app_created = resolve;
-        })
-    }
-    connectedCallback() {
-        if(!this.app) {
-            this.try_create();
-        }
-    }
-
-    try_create() {
-        this.shadowRoot.innerHTML = '';
-
         const base_url = this.getAttribute('baseurl') || window.graph_app_base_url;
-        const mode = this.getAttribute('mode') || '';
-
-        if(base_url === undefined) {
-            return;
-        }
-
-        const link = document.createElement('link');
-        link.setAttribute('rel','stylesheet');
-        link.setAttribute('href',base_url+'style.css');
-        this.shadowRoot.appendChild(link);
-
-        const flags = [base_url, 'blank', mode];
-        const div = document.createElement('div');
-        this.shadowRoot.appendChild(div);
-        observer.observe(div, { childList: true, subtree: true });    
-        var app = this.app = Elm.GraphApp.init({ node: div, flags: flags })
-
-        app.ports.reportState.subscribe(data => {
-            this.state = data;
-            this.dispatchEvent(new CustomEvent('statechanged', { detail: data }));
-        });
-        this.resolve_app_created(app);
+        const scene = this.getAttribute('scene');
+        const mode = this.getAttribute('mode') || 'play hamilton';
+        const flags = [base_url, scene, mode];
+        var app = Elm.Main.init({ node: this, flags: flags })
     }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-        switch(name) {
-            case 'baseurl':
-                this.try_create();
-                break;
-            case 'mode':
-                this.change_setting({mode: newValue});
-                break;
-            case 'changemode':
-                this.change_setting({can_change_mode: truthy_attribute(newValue)});
-                break;
-            case 'disabled':
-                this.change_setting({disabled: truthy_attribute(newValue)});
-                break;
-        }
-    }
-
-    async set_scene(scene) {
-        await this.app_created;
-        this.app.ports.receiveScene.send(scene);
-    }
-
-    async change_setting(settings) {
-        await this.app_created;
-        this.app.ports.settingsPort.send(settings);
-    }
-
-    static get observedAttributes() { return ['baseurl', 'mode', 'changemode','disabled']; }
 }
+
+observer.observe(document.body, { childList: true, subtree: true });    
 
 customElements.define('graph-app', GraphAppElement);
